2928e568b4f3af69d74b4f8e9505bb3635e4302c
jaydenanderson <jaydenanderson@Jaydens-MacBook.local>
2017-04-07 15:41:57 -0700

    Refugees page template.

--- _site/js/index.js
+++ _site/js/index.js
@@ -1,23 +1,21 @@
-// Dropdown Menu
-var dropdown = document.querySelectorAll('.js-toggle');
-var dropdownArray = Array.prototype.slice.call(dropdown,0);
-dropdownArray.forEach(function(el){
-  var button = el.querySelector('.js-toggle-trigger');
+window.onload = function() {
+  // Dropdown Menu
+  var dropdown = document.querySelectorAll('.js-toggle');
+  var dropdownArray = Array.prototype.slice.call(dropdown,0);
+  dropdownArray.forEach(function(el){
+    var button = el.querySelector('.js-toggle-trigger');
 
-  button.onclick = function(event) {
-    this.parentNode.classList.toggle('is-active');
-    event.preventDefault();
-  };
-})
+    button.onclick = function(event) {
+      this.parentNode.classList.toggle('is-active');
+      event.preventDefault();
+    };
+  })
 
-window.onscroll = function() {
-    if ( window.pageYOffset > 100 ) {
-        document.body.classList.add("is-scrolled");
-    } else {
-        document.body.classList.remove("is-scrolled");
-    }
-}
-
-function scrollTo(hash) {
-  location.hash = "#" + hash;
-}
+  window.onscroll = function() {
+      if ( window.pageYOffset > 100 ) {
+          document.body.classList.add("is-scrolled");
+      } else {
+          document.body.classList.remove("is-scrolled");
+      }
+  }
+};
